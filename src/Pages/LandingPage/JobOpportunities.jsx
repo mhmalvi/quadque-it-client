fa1aa8043635e7ded2884392d7862d6c23c74e50
AdilLachin <adillachin929@gmail.com>
2022-11-20 16:41:20 +0600

    JobOpportunity section

---
+++ src/Pages/LandingPage/JobOpportunities.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const JobOpportunities = () => {
+
+    return (
+
+    <div>
+        <div className="font-poppins font-bold text-3xl px-20"> We Bring The Good Education To Life  </div>;
+    </div>
+
+     );
+
+};
+
+export default JobOpportunities;
--- src/Pages/LandingPage/index.jsx
+++ src/Pages/LandingPage/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
+import JobOpportunities from  './JobOpportunities'
 
 function Home() {
   return (
     <div>
       <Navbar/>
+      <JobOpportunities/>
     </div>
   )
+
+  
 }
 
 export default Home
