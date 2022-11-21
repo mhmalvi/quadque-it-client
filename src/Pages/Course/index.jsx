be1e492077ac014124e85c2758e52bc1f8620f10
AdilLachin <adillachin929@gmail.com>
2022-11-21 15:37:47 +0600

    admission pg

--- src/Pages/Course/index.jsx
+++ src/Pages/Course/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../../Components/Shared/Navbar'
-import Header from './Header'
+import Admission from './Admission'
 
-export default function index() {
+export default function index() 
+{
   return (
     <div>
         <Navbar/>
-        <Header/>
+        <Admission/>
     </div>
   )
 }
