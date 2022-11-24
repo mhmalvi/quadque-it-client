4e023a52ddd2bd86cff2da99e23f4fc9088b9332
AdilLachin <adillachin929@gmail.com>
2022-11-24 11:48:26 +0600

    blog detail pg

---
+++ src/Pages/Blog/BlogDetail.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import blogdetail from "../../Asset/Image/blogdetail.png";
+
+const BlogDetail = () => {
+  return (
+    <div className='font-poppins text-white bg-home-color'>
+      <div className=" pt-10 flex justify-center">
+          <img src={blogdetail} alt="" className="rounded-xl" />
+       </div>
+       
+        <div className='mt-8 w-2/3 m-auto font-bold text-3xl'>
+        BLOG DETAILS
+        </div>
+
+        <div className='mt-8 w-2/3 m-auto font-thin text-lg'>
+        Just like graphic design and web development, UX design trends come and go for a <br/>
+         reason. UX is  all about developing a better digital world for humanity. It’s a field that <br/>
+          requires innovation, but  that is constantly evolving. Regularly following UX design blogs <br/>
+           can help you keep on top of  changes in the industry, as well as develop your UX skills.
+        </div>
+
+           
+        <div className='mt-8 w-2/3 m-auto font-bold text-xl'>
+        BLOG DETAILS
+        </div>
+
+        <div className='mx-60 flex relative pl-3 w-1/2 pt-8'>
+
+        <div className=' mt-6 w-2/3  font-extralight text-sm'>
+        Just like graphic design and web development, UX design trends  
+         come and go for a reason. UX is  all about developing a better  
+          digital world for humanity. It’s a field that requires innovation, but  
+            that is constantly evolving. Regularly following UX design blogs   
+             can help you keep on top of  changes in the industry, as well as   
+              develop your UX skills. Just like graphic design and web   
+               development, UX design trends come and go for a reason. UX 
+               is  all about developing a better digital world for humanity. It’s a field  
+                that requires innovation, but  that is constantly evolving. Regularly  
+                 following UX design blogs can help you keep on top of  changes in  
+                  the industry, as well as develop your UX skills.
+
+            </div>
+
+            <div className="  flex w-1/3 ml-8">
+          <img src={blogdetail} alt="" className="rounded-l" />
+       </div>
+       </div>
+
+       <div className='mt-4 w-2/3 m-auto font-extralight text-sm'>
+       Just like graphic design and web development, UX design trends come and go for a reason. 
+       UX is  all about developing a better digital world for humanity. It’s a field that requires innovation, 
+       but  that is constantly evolving. Regularly following UX design blogs can help you keep on top of  changes in the industry, 
+       as well as develop yourUX skills.
+        </div>
+   
+        <div className='mt-8 w-2/3 m-auto font-bold text-xl'>
+        BLOG DETAILS
+        </div>
+
+        <div className='mt-4 w-2/3 m-auto font-extralight text-sm'>
+        Just like graphic design and web development, UX design trends come and go for a reason. UX is  all about 
+         developing a better digital world for humanity. It’s a field that requires innovation, but  that is constantly evolving. 
+         Regularly following UX design blogs can help you keep on top of  changes in the industry, as well as develop 
+         yourUX skills. Just like graphic design and web development, UX design trends come and go for a reason. UX is  
+         all about developing a better digital world for humanity. It’s a field that requires innovation, but  that is constantly 
+         evolving. Regularly following UX design blogs can help you keep on top of  changes in the industry, as well as 
+         develop your UX skills.Just like graphic design and web development, UX design trends come and go for a 
+         reason. UX is  all about developing a better digital world for humanity. It’s a field that requires innovation, but that
+          is constantly evolving. Regularly following UX design blogs can help you keep on top of  changes in the industry, 
+           as well as develop your UX skills. Just like graphic design and web development, UX design trends come and go 
+           for a reason. UX is  all about developing a better digital world for humanity. It’s a field that requires innovation, but  
+          that is constantly evolving. Regularly following UX design blogs can help you keep ontop of  changes in the 
+          industry, as well as develop your UX skills.
+        </div>
+
+
+    </div>
+  )
+}
+
+export default BlogDetail
